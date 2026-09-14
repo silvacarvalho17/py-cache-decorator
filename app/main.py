e3100b7bd924ad75269c7f5d21855c5d2ae37b04
silvacarvalho17 <carvalho.paulojsilva@gmail.com>
2026-09-14 21:41:10 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from functools import wraps
-from typing import Callable, Any
+from typing import Any, Callable
 
 
 def cache(func: Callable) -> Callable:
@@ -23,23 +23,22 @@ def cache(func: Callable) -> Callable:
 
 
 @cache
-def long_time_func(a: int, b: int, c: int) -> int:
-    return (a ** b + c) % (a ** c)
+def long_time_func(
+    first_number: int,
+    second_number: int,
+    third_number: int
+) -> int:
+    return (
+        first_number ** second_number + third_number
+    ) % (first_number ** third_number)
 
 
 @cache
-def long_time_func_2(numbers_tuple: tuple, power: int) -> list:
-    return [number ** power for number in numbers_tuple]
-
-
-long_time_func(2, 2, 3)
-long_time_func(2, 2, 3)
-
-long_time_func_2((5, 6, 7), 5)
-long_time_func_2((5, 6, 7), 5)
-
-long_time_func(1, 2, 3)
-long_time_func(5, 6, 7)
-
-long_time_func_2((5, 6, 7), 10)
-long_time_func_2((5, 6, 7), 10)
+def long_time_func_2(
+    numbers_tuple: tuple,
+    power: int
+) -> list:
+    return [
+        number ** power
+        for number in numbers_tuple
+    ]
